1ab05608c3e1c8b594ca1d35f1fce5259da58b1d
Asief Mahir <asiefmahir1@gmail.com>
2025-09-22 21:23:54 +0600

    feat: Learn about useReducer hook in React

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import TodoProvider from "./contexts/Todo.jsx";
+import App2 from "./App2.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<TodoProvider>
+		{/* <TodoProvider>
 			<App />
-		</TodoProvider>
+		</TodoProvider> */}
+		<App2 />
 	</StrictMode>,
 );
 // react dom -> DOM -> React Code (Component)
