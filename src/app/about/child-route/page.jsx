a0a40b83a5b65d3f1f91e3a0e3c24a2652f79dfc
Asief Mahir <asiefmahir1@gmail.com>
2025-10-27 21:47:27 +0600

    feat: Learn about fundamentals of next js in app router
    Learn about routing mechanism, server component and client component, layouting, nested layouting and rest api calling in server component

---
+++ src/app/about/child-route/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnotherChildRoute = () => {
+	return <div>AnotherChildRoute</div>;
+};
+
+export default AnotherChildRoute;
---
+++ src/app/about/layout.jsx
@@ -0,0 +1,13 @@
+const AboutLayout = ({ children }) => {
+	return (
+		<>
+			{children}
+			<ul>
+				<li>Dummy Li</li>
+				<li>Dummy Li</li>
+			</ul>
+		</>
+	);
+};
+
+export default AboutLayout;
