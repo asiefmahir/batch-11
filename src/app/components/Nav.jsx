103e3c87fe58a246c7a4081de4be903a11e410c1
Asief Mahir <asiefmahir1@gmail.com>
2025-11-08 21:49:11 +0600

    feat: Learn about pagination and searchParams in Next Js

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -10,24 +10,14 @@ const Nav = () => {
 				<li>
 					<Link href="/">Home</Link>
 				</li>
-				<li>
-					<Link href="/about">About</Link>
-				</li>
-				<li>
-					<Link href="/about/dummy">Dummy Page</Link>
-				</li>
-				<li>
-					<Link href="/post-list">Post List</Link>
-				</li>
+
 				<li>
 					<Link href="/shop">Shop</Link>
 				</li>
 				<li>
 					<Link href="/dashboard/admin/add-product">Add product</Link>
 				</li>
-				<li>
-					<Link href="/admin-products">Admin Products</Link>
-				</li>
+
 				<li>
 					<Link href="/register">Sign Up</Link>
 				</li>
--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,45 +1,75 @@
-import ProductCard from "../components/ProductCard";
+import ProductCard from "@/app/components/ProductCard";
+import { getBaseUrl } from "@/app/utils/api";
+import Pagination from "../components/Pagination";
 
-const Shop = async () => {
-	console.log("I am being regenerated");
-	const res = await fetch(`http://localhost:3000/api/product`, {
-		next: { tags: ["products"] },
-	});
-	// fullstack project -> Backend -> Frontend
-	// Backend -> DB
-	// project
+const getProduct = async (searchParams) => {
+	const searchQuery = new URLSearchParams({
+		page: searchParams.page,
+	}).toString();
+	console.log(searchQuery, "searchQuery");
+
+	const baseUrl = getBaseUrl();
+	const res = await fetch(`${baseUrl}/api/product?${searchQuery}`);
+
+	if (!res.ok) {
+		throw new Error(`Failed to Fetch Products`);
+	}
+	// some.com/api?page=2
 	const data = await res.json();
-	console.log(data);
+	return {
+		products: data?.products,
+		totalPages: data?.totalPages,
+		currentPage: data?.currentPage,
+	};
+};
+
+const Shop = async ({ searchParams }) => {
+	const params = await searchParams;
+	console.log("I am being re-rendered");
+	const data = await getProduct(params);
+
+	// console.log("I am being regenerated");
+	// const res = await fetch(`http://localhost:3000/api/product`, {
+	// 	next: { tags: ["products"] },
+	// });
+	// // fullstack project -> Backend -> Frontend
+	// // Backend -> DB
+	// // project
+	// const data = await res.json();
+	// console.log(data);
 
 	return (
-		<div>
-			<div className="page-banner">
-				<div className="page-banner__details">
-					<div className="page-banner__details__title">
-						<h1>Our E-commerce Website</h1>
-					</div>
-				</div>
-			</div>
-			<div className="section">
-				<div className="container">
-					<div className="section__head">
-						<div className="product__details__title">
-							<h2>All Products</h2>
-						</div>
-					</div>
-					<div className="section__content">
-						<div className="grid three">
-							{data?.data.map((product) => (
-								<ProductCard
-									key={product._id}
-									product={product}
-								/>
-							))}
+		<main>
+			<div>
+				<div className="page-banner">
+					<div className="page-banner__details">
+						<div className="page-banner__details__title">
+							<h1>Our E-commerce Website</h1>
 						</div>
 					</div>
 				</div>
+				<div className="section">
+					<div className="container">
+						<div className="section__head">
+							<div className="product__details__title">
+								<h2>All Products</h2>
+							</div>
+						</div>
+						<div className="section__content">
+							<div className="grid three">
+								{data?.products?.map((product) => (
+									<ProductCard
+										key={product._id}
+										product={product}
+									/>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
+			<Pagination totalPages={data.totalPages} pathname={"/shop"} />
+		</main>
 	);
 };
 // pre-rendered html page -> static generated shop html page
@@ -47,3 +77,6 @@ const Shop = async () => {
 // isr -> incrementally static re-generation
 
 export default Shop;
+
+// server side rendering
+// request time rendering
